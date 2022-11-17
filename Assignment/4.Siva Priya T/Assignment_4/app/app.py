e8570f2e815d6a742275f494d40470643ed8e555
sivapriya101 <113841918+sivapriya101@users.noreply.github.com>
2022-11-17 09:41:53 +0530

    Add files via upload

---
+++ Assignment/4.Siva Priya T/Assignment_4/app/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, redirect, url_for, request
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return redirect(url_for('home'))
+
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    error=None
+    if request.method == 'POST' :
+        if request.form['username'] !='dharsh' or request.form['password'] !='dharsh':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('success'))
+    return render_template('login.html',error=error)
+    
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/success")
+def success():
+    return render_template('success.html')
+@app.route("/signupsuccess")
+def signupsuccess():
+    return render_template('signupsuccess.html')
+if __name__=='__main__':
+	app.run(debug=True)
+	
